962486f04981a46f0334f425c258284782406f77
weykent <weykent@weasyl.com>
2014-10-17 01:57:48 -0700

    Pull tags from deviantArt.
    I didn't even know deviantArt had tags!

--- deviantart.js
+++ deviantart.js
@@ -1,10 +1,13 @@
 $waxpost.query(function ($) {
     var title = $('div.dev-title-container h1 a').get(0).innerText;
+    var tags = $.map($('a.discoverytag').get(),
+                     function (x) { return x.getAttribute('data-canonical-tag'); });
     var description = $('div.dev-description').get(0).innerText;
     var imageURL = $('img.dev-content-full').attr('src');
     chrome.runtime.sendMessage({method: 'getSettings'}, function (response) {
         var url = response.settings['waxpost-wzlroot'] + 'submit/visual?' + $.param({
             title: title,
+            tags: tags,
             description: description,
             baseURL: document.location,
             imageURL: imageURL,
