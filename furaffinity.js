f150f953063595a53233e383eded60d44d931acf
Noxid <taeganong@gmail.com>
2015-08-09 12:12:27 -0300

    Change for FA's new layout stuff
    Adds support for "beta" layout
    fixes bug where title would appear as "Owner options"

--- furaffinity.js
+++ furaffinity.js
@@ -1,9 +1,23 @@
 $waxpost.query(function ($) {
-    var title = $('table.maintable table.maintable td.cat b').get(0).innerText;
-    var tags = $.map($('#keywords a').get(),
-                     function (x) { return x.innerText.replace(/ /g, '_'); });
-    var description = $('table.maintable td.alt1[width="70%"]').text();
-    var imageURL = $('a:contains("Download")').attr('href');
+	var title, tags, description, imageURL;
+	if (document.getElementById('submission-details') == null) {
+		//using classic theme
+		title = $('table.maintable table.maintable td.cat b');
+		title = title.get(title.length-1).innerText;
+		tags = $.map($('#keywords a').get(),
+					 function (x) { return x.innerText.replace(/ /g, '_'); });
+		description = $('table.maintable td.alt1[width="70%"]').text();
+	} else {
+		//using beta theme
+		title = $('#submission-details div.desc-col span.fontsize20 b').get(0).innerText;
+		description = $('#submission-details div.desc').text();
+		tags = Array.prototype.map.call(document.getElementsByClassName('tags'), 
+			function(tag) {
+				return tag.getElementsByTagName('a')[0].innerText;
+			});
+		console.log(tags);
+	}
+    imageURL = $('a:contains("Download")').attr('href');
     chrome.runtime.sendMessage({method: 'getSettings'}, function (response) {
         var url = response.settings['waxpost-wzlroot'] + 'submit/visual?' + $.param({
             title: title,
