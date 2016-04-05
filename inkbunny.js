bc67416409e39da3f8b6f261cbc40a54f77e9ab8
Noxid <taeganong@gmail.com>
2016-04-05 12:42:56 -0300

    Update for changes to inkbunny layout
    version changed to 0.3.1

--- inkbunny.js
+++ inkbunny.js
@@ -1,11 +1,10 @@
 $waxpost.query(function ($) {
-    var titlePos = $('div.welcomeanon_userdetails').length ? 2 : 1; //check user login state
-    var title = $('div.content td:nth-child(2) div', $('div.elephant').get(titlePos)).get(0).innerText;
+    var title = document.querySelector("table.pooltable h1").innerText;
     var tags = $('div div:nth-child(1) a span', $('#kw_scroll').next());
     var tags = $.map(tags.get(),
                      function (x) { return x.innerText.replace(/ /g, '_'); });
     var description = $('div.elephant_bottom.elephant_white div.content div').get(0).innerText;
-    var image = $('div.widget_imageFromSubmission img').eq(0);
+    var image = $('img#magicbox').eq(0);
     var imageLink = image.parent('a');
     var imageURL = imageLink.length ? imageLink.attr('href') : image.attr('src');
     chrome.runtime.sendMessage({method: 'getSettings'}, function (response) {
